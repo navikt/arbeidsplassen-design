71eabfe84b6fe85649a7fe9caa26cba78fa7fafb
otenav <ola.toft-eriksen@nav.no>
2022-10-14 11:59:39 +0200

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,24 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.css',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.css'
-        })
+  mode: "production",
+  entry: "./src/index.css",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "arbeidsplassen.css",
+    }),
+  ],
 };
