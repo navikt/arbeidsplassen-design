26718b6b7015b1e36388c731f4a453504157353b
otenav <ola.toft-eriksen@nav.no>
2022-11-08 15:25:17 +0100

    Format and optimize imports

--- stories/LinkPanel.stories.js
+++ stories/LinkPanel.stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import LinkPanel from "../src/LinkPanel/LinkPanel";
-import Container from "../src/Container/Container";
 
 export default {
     title: "Example/LinkPanel",
