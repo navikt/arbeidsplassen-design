878785bb89c984b84f5564a83932baab95884b6f
otenav <ola.toft-eriksen@nav.no>
2022-11-16 11:43:55 +0100

    Add new component Label

---
+++ src/Label/Label.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import joinClassNames from "../joinClassNames";
+
+function Label({ children, className, mini }) {
+    return (
+        <p
+            className={joinClassNames(
+                "dsa-label",
+                mini ? "dsa-label-mini" : undefined,
+                className
+            )}
+        >
+            {children}
+        </p>
+    );
+}
+
+Label.defaultProps = {
+    className: undefined,
+    mini: false,
+};
+
+Label.propTypes = {
+    className: PropTypes.string,
+    mini: PropTypes.bool,
+};
+
+export default Label;
