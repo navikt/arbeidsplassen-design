a5e144c7359a40b3f957136f9d553539f0e64bab
Audun Skjelvan <audun.skjelvan@nav.no>
2022-11-18 13:31:26 +0100

    release:publish

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
-import Accordion from './Accordion/Accordion';
-import Anchor from './Anchor/Anchor';
-import Blockquote from './Blockquote/Blockquote';
-import BodyText from './BodyText/BodyText';
-import Button from './Button/Button';
-import Container from './Container/Container';
-import Figure from './Figure/Figure';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import Heading from './Heading/Heading';
-import Ingress from './Ingress/Ingress';
-import Label from './Label/Label';
-import LinkPanel from './LinkPanel/LinkPanel';
-import List from './List/List';
-import ListEntity from './ListEntity/ListEntity';
-import Panel from './Panel/Panel';
-import Pill from './Pill/Pill';
-import SkipLink from './SkipLink/SkipLink';
-import SmallText from './SmallText/SmallText';
-import TabMenu from './TabMenu/TabMenu';
-import Tag from './Tag/Tag';
-import ToolButton from './ToolButton/ToolButton';
-
-export {Accordion, Anchor, Blockquote, BodyText, Button, Container, Figure, Footer, Header, Heading, Ingress, Label, LinkPanel, List, ListEntity, Panel, Pill, SkipLink, SmallText, TabMenu, Tag, ToolButton}
+export * from './Accordion';
+export * from './Header';
