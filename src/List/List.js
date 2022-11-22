08cf2871b1c60cb1dee6d499ae4b2e7fe2e8e501
Audun Skjelvan <audun.skjelvan@nav.no>
2022-11-22 12:44:33 +0100

    added aria label to list

--- src/List/List.js
+++ src/List/List.js
@@ -2,21 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import joinClassNames from "../.utils/joinClassNames";
 
-function List({ children, variant, className }) {
+function List({ children, variant, className, ariaLabel }) {
     const ListType = variant;
     return (
-        <ListType className={joinClassNames("dsa-list", className)}>
+        <ListType className={joinClassNames("dsa-list", className)} ariaLabel={ariaLabel}>
             {children}
         </ListType>
     );
 }
 
 List.defaultProps = {
+    ariaLabel: undefined,
     className: undefined,
     variant: "ul",
 };
 
 List.propTypes = {
+    ariaLabel: PropTypes.string,
     className: PropTypes.string,
     variant: PropTypes.oneOf(["ul", "ol"]),
 };
