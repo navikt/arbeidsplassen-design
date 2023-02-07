20d08ae093bf48b4cf36f965ce91cc6cb4737906
otenav <ola.toft-eriksen@nav.no>
2023-02-07 13:27:50 +0100

    Chip styles

--- src/pages/Tag.js
+++ src/pages/Tag.js
@@ -9,12 +9,14 @@ const TagExample = () => {
         <Tag variant="success-filled">Success</Tag>
         <Tag variant="warning-filled">Warning</Tag>
         <Tag variant="error-filled">Error</Tag>
+        <Tag variant="neutral-filled">Neutral</Tag>
       </div>
       <div className="flex gap-2 mb-1">
         <Tag variant="info">Info</Tag>
         <Tag variant="success">Success</Tag>
         <Tag variant="warning">Warning</Tag>
         <Tag variant="error">Error</Tag>
+        <Tag variant="neutral">Neutral</Tag>
       </div>
       <div className="flex gap-2 mb-1">
         <Tag variant="info" size="small">
