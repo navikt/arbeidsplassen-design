30608123f51fbc89d355ccfe4a4d5f22ce87ea77
otenav <ola.toft-eriksen@nav.no>
2023-03-07 09:58:03 +0100

    New component SkipLink

--- src/packages/arbeidsplassen-react/Icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MinusCircleIcon } from "./feather/MinusCircleIcon";
-export { default as PlusCircleIcon } from "./feather/PlusCircleIcon";
-export { default as TrashIcon } from "./feather/TrashIcon";
-export { default as UsersIcon } from "./feather/UsersIcon";
