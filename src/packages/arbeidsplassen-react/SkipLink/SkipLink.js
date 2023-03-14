bec2e4ebf4da395a8f710b713f797729e2408fc1
otenav <ola.toft-eriksen@nav.no>
2023-03-14 10:00:25 +0100

    Kaller det "Hopp til innhold"

--- src/packages/arbeidsplassen-react/SkipLink/SkipLink.js
+++ src/packages/arbeidsplassen-react/SkipLink/SkipLink.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SkipLink({ href = "#main", text = "Hopp til hovedinnhold" }) {
+function SkipLink({ href = "#main", text = "Hopp til innhold" }) {
   return (
     <a href={href} className="arb-skip-link">
       {text}
