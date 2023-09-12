953c42c36cdf8fc35351196b70b13baefc7e6674
otenav <ola.toft-eriksen@nav.no>
2023-09-12 11:54:45 +0200

    Legger til ny pakke for theme.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@navikt/ds-css";
 import "../packages/arbeidsplassen-css/index.css";
+import "../packages/arbeidsplassen-theme/index.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
