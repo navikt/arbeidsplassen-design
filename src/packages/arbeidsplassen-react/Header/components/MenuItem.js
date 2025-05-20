324cce95d879d6030edde4dbecebee0e3c23cea4
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-20 08:38:11 +0200

    forward attributes to menuitem

--- src/packages/arbeidsplassen-react/Header/components/MenuItem.js
+++ src/packages/arbeidsplassen-react/Header/components/MenuItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function MenuItem({ href, active, id, children }) {
+function MenuItem({ href, active, id, children, ...rest }) {
   return (
     <a
       href={href}
@@ -9,6 +9,7 @@ function MenuItem({ href, active, id, children }) {
           ? "arb-header-menu-link arb-header-menu-link-active"
           : "arb-header-menu-link"
       }
+      {...rest}
     >
       <span className="arb-header-menu-item-inner">{children}</span>
     </a>
