02f880ca141cf9ff3c1fb87c304296b98229acb0
otenav <29566394+otenav@users.noreply.github.com>
2025-12-03 08:09:52 +0100

    Oppdaterer demo app

--- src/pages/Button.js
+++ src/pages/Button.js
@@ -4,49 +4,40 @@ import { TrashIcon } from "@navikt/aksel-icons";
 import { useState } from "react";
 import PropsCard from "@/examples/PropsCard";
 
-function ButtonTemplate({ variant, size }) {
-    return (
-        <div className="mb-10">
-            <Heading size="small" level="2" spacing>
-                {variant}
-            </Heading>
-            <HStack gap="4">
-                <Button variant={variant} size={size}>
-                    Button
-                </Button>
-                <Button variant={variant} size={size} icon={<TrashIcon />}>
-                    Button
-                </Button>
-                <Button variant={variant} size={size} icon={<TrashIcon />} disabled>
-                    Disabled
-                </Button>
-                <Button variant={variant} size={size} loading>
-                    Laster
-                </Button>
-            </HStack>
-        </div>
-    );
-}
-
 const ButtonExample = () => {
     const [size, setSize] = useState("medium");
+    const [variant, setVariant] = useState("primary");
+    const [loading, setLoading] = useState(false);
 
     return (
         <Layout title="Button">
-            <ButtonTemplate variant="primary" size={size} />
-            <ButtonTemplate variant="primary-neutral" size={size} />
-            <ButtonTemplate variant="secondary" size={size} />
-            <ButtonTemplate variant="secondary-neutral" size={size} />
-            <ButtonTemplate variant="tertiary" size={size} />
-            <ButtonTemplate variant="tertiary-neutral" size={size} />
-            <ButtonTemplate variant="danger" size={size} />
+            <Button variant={variant} size={size} icon={<TrashIcon />} loading={loading}>
+                Button
+            </Button>
 
             <PropsCard>
                 <HStack gap="24">
+                    <RadioGroup legend="variant" value={variant} onChange={setVariant}>
+                        {[
+                            "primary",
+                            "primary-neutral",
+                            "secondary",
+                            "secondary-neutral",
+                            "tertiary",
+                            "tertiary-neutral",
+                            "danger",
+                        ].map((option) => (
+                            <Radio value={option}>{option}</Radio>
+                        ))}
+                    </RadioGroup>
                     <RadioGroup legend="size" value={size} onChange={setSize}>
                         {["medium", "small", "xsmall"].map((option) => (
                             <Radio value={option}>{option}</Radio>
                         ))}
+                    </RadioGroup>
+                    <RadioGroup legend="loading" value={loading} onChange={setLoading}>
+                        <Radio value={false}>false</Radio>
+                        <Radio value={true}>true</Radio>
                     </RadioGroup>
                 </HStack>
             </PropsCard>
